3e624206bda932eb438c9053d556e5a5a5d11581
KHAI PHAN <hphan16@my.bcit.ca>
2025-03-31 15:24:00 -0700

    Declare instance variables for Player Class

--- src/main/java/com/example/comp2522202510termprojectgamename/Player.java
+++ src/main/java/com/example/comp2522202510termprojectgamename/Player.java
@@ -1,4 +1,14 @@
 package com.example.comp2522202510termprojectgamename;
 
-public class Player {
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import java.util.List;
+
+public class Player extends GameObject {
+
+    private static final int WIDTH = 55; // Player image width
+    private static final int HEIGHT = 55; // Player image height
+    private static final double SPEED = 5;
+    private boolean moveLeft;
+    private boolean moveRight;
 }
