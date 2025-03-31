e273b2d305ad8243e7857d0f9c6b444e766c71e5
KHAI PHAN <hphan16@my.bcit.ca>
2025-03-31 15:57:47 -0700

    Add gif variables for Enemy

--- src/main/java/com/example/comp2522202510termprojectgamename/Enemy.java
+++ src/main/java/com/example/comp2522202510termprojectgamename/Enemy.java
@@ -8,4 +8,7 @@ public class Enemy extends GameObject {
     protected static final int WIDTH = 65;
     protected static final int HEIGHT = 65;
     public static double SPEED = 2;
+
+    private static final Image enemyImage = new Image(Enemy.class.getResourceAsStream("/images/junkrat.gif"));
+
 }
