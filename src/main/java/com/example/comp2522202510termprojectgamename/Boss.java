27973104ab45d1fbc1ece5d8c0820bb65c4fc15c
KHAI PHAN <hphan16@my.bcit.ca>
2025-03-31 16:04:22 -0700

    Create Boss class

---
+++ src/main/java/com/example/comp2522202510termprojectgamename/Boss.java
@@ -0,0 +1,12 @@
+package com.example.comp2522202510termprojectgamename;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+public class Boss extends GameObject {
+
+    public static final int WIDTH = 60;
+    public static final int HEIGHT = 60;
+    private static final double SPEED = 5;
+
+}
