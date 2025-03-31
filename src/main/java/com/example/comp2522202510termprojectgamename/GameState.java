b0f1fd21397e409e77311794a90a80855a06f0f5
KHAI PHAN <hphan16@my.bcit.ca>
2025-03-31 14:59:42 -0700

    Create GameState Class

--- src/main/java/com/example/comp2522202510termprojectgamename/GameState.java
+++ src/main/java/com/example/comp2522202510termprojectgamename/GameState.java
@@ -1,5 +1,7 @@
 package com.example.comp2522202510termprojectgamename;
 
-public enum GameState {
-    RUNNING, PAUSED, GAME_OVER, LEVEL_COMPLETE
+import java.util.List;
+import java.util.ArrayList;
+
+public class GameState {
 }
