0476963b13e4491f8c30b920e4a142a9404a0196
KHAI PHAN <hphan16@my.bcit.ca>
2025-03-31 15:04:08 -0700

    Add accessors and mutators for GameState Class

--- src/main/java/com/example/comp2522202510termprojectgamename/GameState.java
+++ src/main/java/com/example/comp2522202510termprojectgamename/GameState.java
@@ -4,4 +4,56 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class GameState {
+    public static final int WIDTH = 400;
+    public static final int HEIGHT = 800;
+
+    private int numLives = 10;
+    private int score = 0;
+    private boolean reset = false;
+
+    private final List<GameObject> gameObjects = new ArrayList<>();
+    private final List<GameObject> newObjects = new ArrayList<>();
+
+    private final Player player = new Player(WIDTH / 2, HEIGHT - 40);
+
+    public GameState() {
+        gameObjects.add(player);
+    }
+
+    public int getNumLives() {
+        return numLives;
+    }
+
+    public void setNumLives(int numLives) {
+        this.numLives = numLives;
+    }
+
+    public int getScore() {
+        return score;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public boolean isReset() {
+        return reset;
+    }
+
+    public void setReset(boolean reset) {
+        this.reset = reset;
+    }
+
+    public List<GameObject> getGameObjects() {
+        return gameObjects;
+    }
+
+    public List<GameObject> getNewObjects() {
+        return newObjects;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+
 }
