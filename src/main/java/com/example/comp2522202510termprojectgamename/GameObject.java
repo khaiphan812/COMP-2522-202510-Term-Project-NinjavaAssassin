cd97c93cde63d588da6bd298a841a447a49dfabb
KHAI PHAN <hphan16@my.bcit.ca>
2025-03-31 14:45:21 -0700

    Create GameObject abstract class

---
+++ src/main/java/com/example/comp2522202510termprojectgamename/GameObject.java
@@ -0,0 +1,10 @@
+package com.example.comp2522202510termprojectgamename;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.geometry.Bounds;
+import javafx.scene.shape.Rectangle;
+
+public abstract class GameObject {
+
+
+}
