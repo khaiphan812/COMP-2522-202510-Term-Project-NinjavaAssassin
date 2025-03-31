5304ccaff4d447236f6912dd2b2ea918ee4fccc4
KHAI PHAN <hphan16@my.bcit.ca>
2025-03-31 16:22:44 -0700

    Create constructor for GameUI

--- src/main/java/com/example/comp2522202510termprojectgamename/GameUI.java
+++ src/main/java/com/example/comp2522202510termprojectgamename/GameUI.java
@@ -19,4 +19,23 @@ public class GameUI {
     private final Pane root;
     private final GameState gameState;
 
+    public GameUI(Pane root, GameState gameState) {
+        this.root = root;
+        this.gameState = gameState;
+
+        scoreLabel = new Label("Score: 0");
+        scoreLabel.setTranslateX(10);
+        scoreLabel.setTranslateY(10);
+        scoreLabel.setTextFill(Color.WHITESMOKE);
+        scoreLabel.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 18));
+
+        lifeLabel = new Label("Lives: 10");
+        lifeLabel.setTranslateX(10);
+        lifeLabel.setTranslateY(40);
+        lifeLabel.setTextFill(Color.WHITESMOKE);
+        lifeLabel.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 18));
+
+        root.getChildren().addAll(scoreLabel, lifeLabel);
+    }
+
 }
