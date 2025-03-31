41a4dcc6683ebaeb0f962ffde543d22e897c470b
KHAI PHAN <hphan16@my.bcit.ca>
2025-03-31 15:57:25 -0700

    Declare instance variables for Enemy

--- src/main/java/com/example/comp2522202510termprojectgamename/Enemy.java
+++ src/main/java/com/example/comp2522202510termprojectgamename/Enemy.java
@@ -1,4 +1,11 @@
 package com.example.comp2522202510termprojectgamename;
 
-public class Enemy {
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+public class Enemy extends GameObject {
+
+    protected static final int WIDTH = 65;
+    protected static final int HEIGHT = 65;
+    public static double SPEED = 2;
 }
