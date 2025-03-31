f0e8035fd8f19b1858a35ae08a5671447447eba2
KHAI PHAN <hphan16@my.bcit.ca>
2025-03-31 14:47:40 -0700

    Create instance variables and constructor for GameObject

--- src/main/java/com/example/comp2522202510termprojectgamename/GameObject.java
+++ src/main/java/com/example/comp2522202510termprojectgamename/GameObject.java
@@ -6,5 +6,17 @@ import javafx.scene.shape.Rectangle;
 
 public abstract class GameObject {
 
+    protected double xCoordinate;
+    protected double yCoordinate;
+    protected double width;
+    protected double height;
+
+    public GameObject(double xCoordinate, double yCoordinate, double width, double height) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+        this.width = width;
+        this.height = height;
+    }
+
 
 }
