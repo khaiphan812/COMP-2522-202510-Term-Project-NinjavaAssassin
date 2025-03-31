2bb5557238b355ef034ab714e838fc912e535397
KHAI PHAN <hphan16@my.bcit.ca>
2025-03-31 15:36:13 -0700

    Declare instance variables for Shuriken Class

--- src/main/java/com/example/comp2522202510termprojectgamename/Shuriken.java
+++ src/main/java/com/example/comp2522202510termprojectgamename/Shuriken.java
@@ -1,4 +1,12 @@
 package com.example.comp2522202510termprojectgamename;
 
-public class Shuriken {
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+public class Shuriken extends GameObject {
+
+    public static final int WIDTH = 25;
+    public static final int HEIGHT = 25;
+    private static final double SPEED = 7;
+
 }
