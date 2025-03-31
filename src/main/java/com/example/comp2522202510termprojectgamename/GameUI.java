dbc37d9d5df73fec5152c4d6e9e04cf83a8fc041
KHAI PHAN <hphan16@my.bcit.ca>
2025-03-31 16:17:10 -0700

    Create GameUI Class

---
+++ src/main/java/com/example/comp2522202510termprojectgamename/GameUI.java
@@ -0,0 +1,22 @@
+package com.example.comp2522202510termprojectgamename;
+
+import javafx.application.Platform;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class GameUI {
+    private final Label scoreLabel;
+    private final Label lifeLabel;
+    private final Pane root;
+    private final GameState gameState;
+
+}
