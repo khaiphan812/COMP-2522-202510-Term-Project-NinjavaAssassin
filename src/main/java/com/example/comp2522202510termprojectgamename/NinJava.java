ff8d232d9326bcfcbfcc6f664474c206c629cefb
KHAI PHAN <hphan16@my.bcit.ca>
2025-03-31 23:03:39 -0700

    Declare instance variables for NinJava

--- src/main/java/com/example/comp2522202510termprojectgamename/NinJava.java
+++ src/main/java/com/example/comp2522202510termprojectgamename/NinJava.java
@@ -1,4 +1,19 @@
 package com.example.comp2522202510termprojectgamename;
 
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
 public class NinJava {
+
+    private GameState gameState;
+    private GameUI gameUI;
+    private GameLogic gameLogic;
+    private ImageView playerView;
 }
