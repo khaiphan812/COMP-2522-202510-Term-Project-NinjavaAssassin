a118ad066aefeed8b604a7328345cad1accf3926
KHAI PHAN <hphan16@my.bcit.ca>
2025-04-01 23:36:04 -0700

    Add key event handler

--- src/main/java/com/example/comp2522202510termprojectgamename/NinJava.java
+++ src/main/java/com/example/comp2522202510termprojectgamename/NinJava.java
@@ -31,4 +31,28 @@ public class NinJava extends Application {
         bgView.setFitHeight(GameState.HEIGHT);
         root.getChildren().add(0, bgView);
     }
+
+    private void initEventHandlers(final Scene scene) {
+        Player player = gameState.getPlayer();
+        scene.setOnKeyPressed(event -> {
+            switch (event.getCode()) {
+                case A: case LEFT: player.setMoveLeft(true); break;
+                case D: case RIGHT: player.setMoveRight(true); break;
+                case W: case UP: player.setMoveUp(true); break;
+                case S: case DOWN: player.setMoveDown(true); break;
+                case SPACE: player.shoot(gameState.getNewObjects()); break;
+                default: break;
+            }
+        });
+        scene.setOnKeyReleased(event -> {
+            switch (event.getCode()) {
+                case A: case LEFT: player.setMoveLeft(false); break;
+                case D: case RIGHT: player.setMoveRight(false); break;
+                case W: case UP: player.setMoveUp(false); break;
+                case S: case DOWN: player.setMoveDown(false); break;
+                default: break;
+            }
+        });
+    }
+
 }
