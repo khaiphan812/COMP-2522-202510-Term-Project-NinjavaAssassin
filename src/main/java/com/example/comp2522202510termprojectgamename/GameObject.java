be41294fb9f401163df9c0b90658ae89b75f5b51
KHAI PHAN <hphan16@my.bcit.ca>
2025-04-02 15:00:06 -0700

    Create hashCode, equals, toString methods for GameObject

--- src/main/java/com/example/comp2522202510termprojectgamename/GameObject.java
+++ src/main/java/com/example/comp2522202510termprojectgamename/GameObject.java
@@ -3,6 +3,7 @@ package com.example.comp2522202510termprojectgamename;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.geometry.Bounds;
 import javafx.scene.shape.Rectangle;
+import java.util.Objects;
 
 public abstract class GameObject {
 
@@ -29,9 +30,33 @@ public abstract class GameObject {
         return yCoordinate; }
 
     public Bounds getBounds() {
-        return new Rectangle(xCoordinate - getWidth() / 2, yCoordinate - getHeight() / 2, getWidth(), getHeight()).getBoundsInLocal();
+        return new Rectangle(xCoordinate - getWidth() / 2,
+                yCoordinate - getHeight() / 2, getWidth(), getHeight()).getBoundsInLocal();
     }
     public abstract double getWidth();
 
     public abstract double getHeight();
+
+    @Override
+    public boolean equals(final Object object) {
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        GameObject that = (GameObject) object;
+        return Double.compare(xCoordinate, that.xCoordinate) == 0
+                && Double.compare(yCoordinate, that.yCoordinate) == 0
+                && Double.compare(width, that.width) == 0
+                && Double.compare(height, that.height) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(xCoordinate, yCoordinate, width, height);
+    }
+
+    @Override
+    public String toString() {
+        return "GameObject{" + "xCoordinate=" + xCoordinate + ", yCoordinate=" + yCoordinate
+                + ", width=" + width + ", height=" + height + '}';
+    }
 }
