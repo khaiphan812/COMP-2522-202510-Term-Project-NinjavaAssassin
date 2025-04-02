0022a4e104d58655ba7f68a92bc5ed82a2bf193d
KHAI PHAN <hphan16@my.bcit.ca>
2025-04-01 22:54:01 -0700

    Resolve some checkstyle warnings for GameUI

--- src/main/java/com/example/comp2522202510termprojectgamename/GameUI.java
+++ src/main/java/com/example/comp2522202510termprojectgamename/GameUI.java
@@ -7,42 +7,43 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class GameUI {
+    public static final int DELAY = 2500;
+    public static final int SETTING_1 = 10;
+    public static final int SETTING_2 = 40;
     private final Label scoreLabel;
     private final Label lifeLabel;
     private final Pane root;
     private final GameState gameState;
 
-    public GameUI(Pane root, GameState gameState) {
+    public GameUI(final Pane root, final GameState gameState) {
         this.root = root;
         this.gameState = gameState;
 
         scoreLabel = new Label("Score: 0");
-        scoreLabel.setTranslateX(10);
-        scoreLabel.setTranslateY(10);
+        scoreLabel.setTranslateX(SETTING_1);
+        scoreLabel.setTranslateY(SETTING_1);
         scoreLabel.setTextFill(Color.WHITESMOKE);
         scoreLabel.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 18));
 
         lifeLabel = new Label("Lives: 10");
-        lifeLabel.setTranslateX(10);
-        lifeLabel.setTranslateY(40);
+        lifeLabel.setTranslateX(SETTING_1);
+        lifeLabel.setTranslateY(SETTING_2);
         lifeLabel.setTextFill(Color.WHITESMOKE);
         lifeLabel.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 18));
 
         root.getChildren().addAll(scoreLabel, lifeLabel);
     }
 
-    public void updateScore(int score) {
+    public void updateScore(final int score) {
         scoreLabel.setText("Score: " + score);
     }
 
-    public void updateLives(int lives) {
+    public void updateLives(final int lives) {
         lifeLabel.setText("Lives: " + lives);
     }
 
@@ -62,7 +63,7 @@ public class GameUI {
                                 root.getChildren().remove(lostMessage));
                     }
                 },
-                2500
+                DELAY
         );
     }
 }
