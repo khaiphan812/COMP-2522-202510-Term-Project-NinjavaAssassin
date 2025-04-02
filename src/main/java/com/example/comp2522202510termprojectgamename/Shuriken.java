c30000ca145a2ffa272071f366535c5e0c0edd0f
KHAI PHAN <hphan16@my.bcit.ca>
2025-04-02 15:09:02 -0700

    Improve readability for Shuriken

--- src/main/java/com/example/comp2522202510termprojectgamename/Shuriken.java
+++ src/main/java/com/example/comp2522202510termprojectgamename/Shuriken.java
@@ -7,8 +7,8 @@ import java.util.Objects;
 
 public class Shuriken extends GameObject {
 
-    public static final int WIDTH = 25;
-    public static final int HEIGHT = 25;
+    protected static final int WIDTH = 25;
+    protected static final int HEIGHT = 25;
     private static final double SPEED = 7;
     private static final Image SHURIKEN_IMAGE = new Image(Objects.requireNonNull(Shuriken.class.
             getResourceAsStream("/images/grey.gif")));
