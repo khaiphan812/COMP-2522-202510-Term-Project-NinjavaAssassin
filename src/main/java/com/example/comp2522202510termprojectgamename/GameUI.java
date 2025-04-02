297d1127157fbb6133d545b06164480ff8e8b075
KHAI PHAN <hphan16@my.bcit.ca>
2025-04-01 23:49:20 -0700

    Remove magic numbers in GameUI

--- src/main/java/com/example/comp2522202510termprojectgamename/GameUI.java
+++ src/main/java/com/example/comp2522202510termprojectgamename/GameUI.java
@@ -15,6 +15,7 @@ public class GameUI {
     private static final int DELAY = 2500;
     private static final int SETTING_1 = 10;
     private static final int SETTING_2 = 40;
+    private static final int FONT_SIZE = 18;
     private final Label scoreLabel;
     private final Label lifeLabel;
     private final Pane root;
@@ -28,13 +29,13 @@ public class GameUI {
         scoreLabel.setTranslateX(SETTING_1);
         scoreLabel.setTranslateY(SETTING_1);
         scoreLabel.setTextFill(Color.WHITESMOKE);
-        scoreLabel.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 18));
+        scoreLabel.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, FONT_SIZE));
 
         lifeLabel = new Label("Lives: 10");
         lifeLabel.setTranslateX(SETTING_1);
         lifeLabel.setTranslateY(SETTING_2);
         lifeLabel.setTextFill(Color.WHITESMOKE);
-        lifeLabel.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 18));
+        lifeLabel.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, FONT_SIZE));
 
         root.getChildren().addAll(scoreLabel, lifeLabel);
     }
@@ -48,11 +49,12 @@ public class GameUI {
     }
 
     public void showGameOverMessage() {
-        Text lostMessage = new Text("Game Over! Your score is " + gameState.getScore() + ".\nGame has been reset.");
-        lostMessage.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 18));
+        Text lostMessage = new Text("Game Over! Your score is "
+                + gameState.getScore() + ".\nGame has been reset.");
+        lostMessage.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, FONT_SIZE));
         lostMessage.setFill(Color.WHITESMOKE);
         lostMessage.setX((GameState.WIDTH - lostMessage.getLayoutBounds().getWidth()) / 2);
-        lostMessage.setY(GameState.HEIGHT / 2);
+        lostMessage.setY((double) GameState.HEIGHT / 2);
         root.getChildren().add(lostMessage);
 
         new Timer().schedule(
