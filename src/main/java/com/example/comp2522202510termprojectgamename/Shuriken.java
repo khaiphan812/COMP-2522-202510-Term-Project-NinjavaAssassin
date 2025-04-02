dc738372dd5388d99d134553659bd1d518860fbb
KHAI PHAN <hphan16@my.bcit.ca>
2025-04-02 00:50:14 -0700

    Fix type casting for Shuriken

--- src/main/java/com/example/comp2522202510termprojectgamename/Shuriken.java
+++ src/main/java/com/example/comp2522202510termprojectgamename/Shuriken.java
@@ -3,15 +3,17 @@ package com.example.comp2522202510termprojectgamename;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
+import java.util.Objects;
+
 public class Shuriken extends GameObject {
 
     public static final int WIDTH = 25;
     public static final int HEIGHT = 25;
     private static final double SPEED = 7;
+    private static final Image SHURIKEN_IMAGE = new Image(Objects.requireNonNull(Shuriken.class.
+            getResourceAsStream("/images/grey.gif")));
 
-    private static final Image shurikenImage = new Image(Shuriken.class.getResourceAsStream("/images/grey.gif"));
-
-    public Shuriken(double xCoordinate, double yCoordinate) {
+    public Shuriken(final double xCoordinate, final double yCoordinate) {
         super(xCoordinate, yCoordinate, WIDTH, HEIGHT);
     }
 
@@ -31,13 +33,14 @@ public class Shuriken extends GameObject {
     }
 
     @Override
-    public void render(GraphicsContext gc) {
-        gc.drawImage(shurikenImage, xCoordinate - WIDTH / 2, yCoordinate - HEIGHT / 2, WIDTH, HEIGHT);
+    public void render(final GraphicsContext gc) {
+        gc.drawImage(SHURIKEN_IMAGE, xCoordinate - (double) WIDTH / 2,
+                yCoordinate - (double) HEIGHT / 2, WIDTH, HEIGHT);
     }
 
     private boolean dead = false;
 
-    public void setDead(boolean dead) {
+    public void setDead(final boolean dead) {
         this.dead = dead;
     }
 
