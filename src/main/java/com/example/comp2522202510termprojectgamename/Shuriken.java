586fb5de542ebe16b7852cb9e162313f75ce26ce
KHAI PHAN <hphan16@my.bcit.ca>
2025-04-03 13:48:30 -0700

    Add helpful Javadoc for Shuriken class and constructor

--- src/main/java/com/example/comp2522202510termprojectgamename/Shuriken.java
+++ src/main/java/com/example/comp2522202510termprojectgamename/Shuriken.java
@@ -4,14 +4,30 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import java.util.Objects;
 
+/**
+ * Represents the shuriken in the game.
+ *
+ * @author Khai Phan
+ * @version 2025
+ */
 public class Shuriken extends GameObject {
-
+    /**
+     * Width is an int.
+     */
     protected static final int WIDTH = 25;
+    /**
+     * Height is an int.
+     */
     protected static final int HEIGHT = 25;
     private static final double SPEED = 7;
     private static final Image SHURIKEN_IMAGE = new Image(Objects.requireNonNull(Shuriken.class.
             getResourceAsStream("/images/grey.gif")));
-
+    /**
+     * Constructs a new shuriken.
+     *
+     * @param xCoordinate the X coordinate of the shuirken
+     * @param yCoordinate the Y coordinate of the shuirken
+     */
     public Shuriken(final double xCoordinate, final double yCoordinate) {
         super(xCoordinate, yCoordinate, WIDTH, HEIGHT);
     }
